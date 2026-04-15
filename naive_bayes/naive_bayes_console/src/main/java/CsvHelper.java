316c9442fda49db10a0ec90a0b60bcf239df635d
Igo Marcelino <geslucian@hotmail.com>
2026-04-14 21:48:23 -0300

    feat: Aprendendo o vacabulario a partir do CSV

--- naive_bayes/naive_bayes_console/src/main/java/CsvHelper.java
+++ naive_bayes/naive_bayes_console/src/main/java/CsvHelper.java
@@ -1,27 +1,81 @@
 import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvValidationException;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 public class CsvHelper {
 
-    public List<String> words(Path filePath) {
-        List<String> words_reads = new ArrayList<>();
+    boolean firstLine = true;
+
+    /**
+     * @param filePath   Aprender o vocabulario a partir do CSV informado
+     * */
+    public List<String> vocabulario(Path filePath) {
+        List<String>  volcabulario = new ArrayList<>();
         try (CSVReader csvReader = new CSVReader(new FileReader(new File(filePath.toUri())))) {
             String[] line;
             while ((line = csvReader.readNext()) != null) {
-
-                words_reads.add(Arrays.asList(line).toString());
+                if (firstLine){ // pular o cabecalho
+                    firstLine = false;
+                    continue;
+                }
+                for (String s: line){
+                    String cleaned = s.replaceAll("[^\\p{L} ]", " ");
+                    String[] palavras = cleaned.split("\\s+");
+                    for (String palavra: palavras){
+                        String palavraNormalizada = palavra.toLowerCase().trim();
+                        if (!stopWords.contains(palavraNormalizada)){
+                            volcabulario.add(palavra);
+                        }
+                    }
+                }
             }
         } catch (Exception e) {
         }
-        return words_reads;
+        return volcabulario;
     }
+
+    /**
+     * @implNote Palavras para serem ignoradas para evitar acumulo de notas neutrras
+     * */
+    private final Set<String> stopWords = Set.of(
+            "a","à","às","ao","aos",
+            "o","os","as",
+            "um","uma","uns","umas",
+            "de","da","das","do","dos",
+            "em","no","na","nos","nas",
+            "por","para","com","sem","sob","sobre","entre","até","após","antes","desde",
+            "e","ou","mas","porque","porquê","por que","se","como","quando","onde","que",
+            "eu","tu","ele","ela","nós","vós","eles","elas",
+            "me","te","lhe","lhes","vos",
+            "meu","minha","meus","minhas",
+            "teu","tua","teus","tuas",
+            "seu","sua","seus","suas",
+            "este","esta","estes","estas",
+            "esse","essa","esses","essas",
+            "aquele","aquela","aqueles","aquelas",
+            "já","ainda","também","só","apenas","muito","muitos","muita","muitas",
+            "pouco","poucos","pouca","poucas",
+            "mais","menos","tão","tanto","tanta","tantos","tantas",
+            "sim","não","nao",
+            "ser","é","são","era","eram","sendo","será","serão",
+            "estar","está","estão","estava","estavam","esteve","estiveram",
+            "ter","tem","têm","tinha","tinham","teve","tiveram",
+            "haver","há","haviam",
+            "fazer","faz","fazem","fez","fazendo",
+            "dizer","diz","disse","dizem",
+            "ir","vai","vão","foi","foram",
+            "mesmo","mesma","mesmos","mesmas",
+            "outro","outra","outros","outras",
+            "algo","alguém","algum","alguma","alguns","algumas",
+            "cada","todo","toda","todos","todas",
+            "aqui","ali","lá","cá",
+            "isso","isto","aquilo",
+            "etc","etc."
+    );
+
 }
--- naive_bayes/naive_bayes_console/src/main/java/Main.java
+++ naive_bayes/naive_bayes_console/src/main/java/Main.java
@@ -1,19 +1,16 @@
 import java.nio.file.Path;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
         CsvHelper helper = new CsvHelper();
 
-        var palavra = helper.words(Path.of("src/main/resources/golden_base.csv"));
+        var palavra = helper.vocabulario(Path.of("src/main/resources/golden_base.csv"));
 
 
-        for(String s: palavra){
-            List<String> newList = List.of(s.split(","));
 
 
-        }
+        System.out.println(palavra);
 
 
 
